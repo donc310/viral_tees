4e8125e801f4d709dbf541e6eb4f576928cff323
Mitchell Bregman <mitchbregs@gmail.com>
2019-04-13 21:00:15 -0400

    cleaned up

--- notebooks/mitch/4.1-gmail-integration.py
+++ notebooks/mitch/4.1-gmail-integration.py
@@ -10,10 +10,8 @@ class Gmail(object):
         self.server = 'smtp.gmail.com'
         self.port = 465
         session = smtplib.SMTP_SSL(self.server, self.port)
-        session.ehlo
         session.login(self.email, self.password)
         self.session = session
-        print('Connected to Gmail account successfully.')
 
     def send_message(self, subject, body):
         headers = [
@@ -27,26 +25,3 @@ class Gmail(object):
             self.email,
             self.recepient,
             headers + "\r\n\r\n" + body)
-        print('- Message has been sent.')
-
-df = pd.read_csv('MMC Brochure.csv', error_bad_lines=False)
-
-for index, row in df.iterrows():
-    print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
-    comp_name = str(row[' Company'])
-    print('Email to: ' + comp_name)
-    per_name = str(row[' FirstName'])
-    print('Email to: ' + per_name)
-    rec = str(row[' Email'])
-    print('Email to: ' + rec)
-    message_body  = 'Hi ' + per_name +',<br><br>We\'re excited to be hosting our Chicago Hotel Investment & Development event on October 10th. In doing some research, I feel that ' + comp_name + ' would be a great fit for one of our panels.<br><br>Are you free anytime this week or next for a call to discuss the opportunity?<br><br>Best, <br><br>Sam Markovich'
-
-    gm = Gmail('sam.markovich@bisnow.com', 'pass', rec)
-    gm.send_message('Bisnow Meeting: ' + per_name + ' - Hotel Investment & Development Event', message_body)
-    print('-- Message for ' + rec + ' (' + comp_name + ') is completed.')
-
-print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
-print('*********************************')
-print('Finish reading through CSV.')
-print('*********************************')
-print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
