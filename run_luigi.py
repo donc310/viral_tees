e8a62ae7a3d64e7beb218f7211e0fd9e5a97ec12
Mitchell Bregman <mitchbregs@gmail.com>
2019-04-25 22:35:21 -0400

    non working luigi pipeline, trying to implement kwargs

--- run_luigi.py
+++ run_luigi.py
@@ -1,24 +1,47 @@
 import luigi
 from datetime import datetime
 
-class QueryTwitterTrends(luigi.ExternalTask):
-    import pandas as pd
-    from retrieve_trends import run as retrieve_trends
-
-    args_dict = {
-        'location': ['usa-nyc']
-    }
-
-    date = datetime.now()
-    str_date = date.strftime('%m%d_%Y_%H%M')
+class QueryTwitterTrends(luigi.Task):
 
     def requires(self):
         return []
  
-    def output(self):
-        return luigi.LocalTarget("data/trends/trends_{}.csv".format(str_date))
+    def output(self, **kwargs):
+        date = datetime.now()
+        str_date = date.strftime('%m%d_%Y_%H%M')
+        
+        return luigi.LocalTarget("data/trends/trends_{}_{}.csv".format(kwargs, str_date))
  
     def run(self):
-        json_data, df_data = retrieve_trends(args_dict)
-        df_data.to_csv(self.output())
-    
+        from retrieve_trends import run as retrieve_trends
+        import pandas as pd
+
+        locations = [
+                'usa-nyc', 
+                'usa-lax', 
+                'usa-chi', 
+                'usa-dal',
+                'usa-hou',
+                'usa-wdc',
+                'usa-mia',
+                'usa-phi',
+                'usa-atl',
+                'usa-bos',
+                'usa-phx',
+                'usa-sfo',
+                'usa-det',
+                'usa-sea',
+        ]
+
+        for loc in locations:
+            args_dict = {
+                'location': [loc]
+            }
+
+            json_data, df_data = retrieve_trends(args_dict)
+            f = self.output(loc).open('w')
+            df_data.to_csv(f, sep=',', encoding='utf-8')
+            f.close()
+   
+if __name__ == '__main__':
+    luigi.run()
